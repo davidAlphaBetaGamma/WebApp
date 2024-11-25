6c61f26f9eba9f39c649af2912918c670da8e15c
davidAlphaBetaGamma <david.gerling@fra-uas.de>
2024-11-26 00:47:08 +0100

    #

--- Uebung2/SpringIoC/src/main/java/edu/fra/uas/v3autowired/ConstructorMan.java
+++ Uebung2/SpringIoC/src/main/java/edu/fra/uas/v3autowired/ConstructorMan.java
@@ -2,16 +2,17 @@ package edu.fra.uas.v3autowired;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-
 import org.springframework.stereotype.Component;
 
 @Component
-public class Journeyman {
+public class ConstructorMan {
+
     @Autowired
-    @Qualifier("pleasePaint")
+    @Qualifier("Building house")
     Work work;
 
     public void performWork() {
         work.doWork();
     }
+
 }
